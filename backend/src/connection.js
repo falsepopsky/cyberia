15c59d79bfa3dbea748d4751da79d7d78974fb92
falsepopsky <falsepopsky@protonmail.com>
2022-12-02 22:42:34 -0300

    deps: use mysql2

--- backend/src/connection.js
+++ backend/src/connection.js
@@ -1,17 +1,19 @@
-const mysql = require('mysql');
-const { MYSQL_USER, MYSQL_HOST, MYSQL_PASSWORD, MYSQL_DATABASE } = process.env;
+const mysql = require('mysql2');
+const { MYSQL_USER, MYSQL_HOST, MYSQL_PASSWORD, MYSQL_DATABASE, MYSQL_PORT } =
+  process.env;
 
 let conexion = mysql.createConnection({
   host: MYSQL_HOST,
   user: MYSQL_USER,
   password: MYSQL_PASSWORD,
   database: MYSQL_DATABASE,
+  port: MYSQL_PORT,
 });
 
 conexion.connect((err) => {
   if (err) {
-    console.error('Error al conectarse a la DB');
-    return;
+    console.error(err);
+    console.log('Error al conectarse a la DB');
   } else {
     console.log('Conectado con exito a la DB!');
   }
