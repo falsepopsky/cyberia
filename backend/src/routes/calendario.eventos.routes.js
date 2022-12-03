417019f109d3ff559961fb4a2af271b3d70c7e92
falsepopsky <falsepopsky@protonmail.com>
2022-12-03 00:04:54 -0300

    chore: remove old code

--- backend/src/routes/calendario.eventos.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const { calendarioRoutes } = require('../controllers/calendario_controller');
-
-router.get('/api/calendario', calendarioRoutes);
-
-module.exports = router;
