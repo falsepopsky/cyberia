f44afa151d0c8f1c5ef80c58f9870901d670a5c4
falsepopsky <falsepopsky@protonmail.com>
2023-08-23 19:16:23 -0300

    deps: fix db types

---
+++ packages/db/index.d.ts
@@ -0,0 +1,3 @@
+import { PrismaClient } from '@prisma/client';
+export declare const prisma: PrismaClient<import(".prisma/client").Prisma.PrismaClientOptions, never, import("@prisma/client/runtime/library.js").DefaultArgs>;
+export * from '@prisma/client';
--- packages/db/index.js
+++ packages/db/index.js
@@ -1,14 +1,9 @@
 import { PrismaClient } from '@prisma/client';
-
 const globalForPrisma = global;
-
-/** @type {PrismaClient} */
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
-  });
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
-
+export const prisma = globalForPrisma.prisma ||
+    new PrismaClient({
+        log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+    });
+if (process.env.NODE_ENV !== 'production')
+    globalForPrisma.prisma = prisma;
 export * from '@prisma/client';
