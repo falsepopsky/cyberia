6f2560dced3b930c6615d76cf1985ca98b261df7
falsepopsky <falsepopsky@protonmail.com>
2024-02-10 04:38:46 -0300

    fix: format files

--- packages/db/index.d.ts
+++ packages/db/index.d.ts
@@ -1,3 +1,7 @@
 import { PrismaClient } from '@prisma/client';
-export declare const prisma: PrismaClient<import(".prisma/client").Prisma.PrismaClientOptions, never, import("@prisma/client/runtime/library.js").DefaultArgs>;
+export declare const prisma: PrismaClient<
+  import('.prisma/client').Prisma.PrismaClientOptions,
+  never,
+  import('@prisma/client/runtime/library.js').DefaultArgs
+>;
 export * from '@prisma/client';
